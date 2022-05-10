a78b541551ecddb58f293625c4bc9cb1fb526ccc
Franco Rod <francorodios@gmail.com>
2022-05-10 15:14:30 -0400

    Share Button phase 1

--- Sharebutton/ViewController.swift
+++ Sharebutton/ViewController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Button
+    private let button: UIButton = {
+    let button = UIButton()
+    button.backgroundColor = .link
+    button.setTitle("Tap Me", for: .normal)
+    button.setTitleColor(.white, for: .normal)
+        return button
 
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        button.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        button.center = view.center
+        button.addTarget(self, action: #selector(presentShareSheet), for: .touchUpInside)
+        view.addSubview(button)
     }
+    
+    @objc private func presentShareSheet() {
+        guard let image = UIImage(systemName: "bell"), let url = URL(string: "https://www.apple.com") else {
+            return
+        }
+        let shareSheetVC = UIActivityViewController(activityItems: [image, url], applicationActivities: nil)
+        present(shareSheetVC, animated: true)
+    }
+    
 
 
 }
